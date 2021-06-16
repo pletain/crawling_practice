561bad48c5a0520ed28ac50faebe12a74dbdc849
김태형 <thk12378905@gmail.com>
2021-06-16 05:58:12 +0000

    adress modify

--- node_crawling/request.js
+++ node_crawling/request.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 // 네이버 주소 가져오기
-const url = "www.naver.com";
+const url = "https://www.naver.com/";
 
 request(url, (error, response, body) => {  
     console.log(body);
